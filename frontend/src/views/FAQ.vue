e04a54196f527d740e52bb4bea220cdb903e7b13
John Powers <john.powers070@gmail.com>
2020-08-23 15:09:00 -0400

    great

--- frontend/src/views/FAQ.vue
+++ frontend/src/views/FAQ.vue
@@ -1,3 +1,16 @@
 <template>
-  <div>FAQ</div>
+  <div>
+    <div class="faq">
+      <p>Dokimi is a demo site for Vue.js and Flask</p>
+      <p>Use the top menu to navigate to the dashboard</p>
+      <p>From there, follow the directions on the left menu</p>
+    </div>
+  </div>
 </template>
+<style scoped>
+.faq {
+  text-align: center;
+  margin-top: 300px;
+  font-size: 1em;
+}
+</style>
